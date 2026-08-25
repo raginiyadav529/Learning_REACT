a5475174cdfe70bf56296ff833103250b6033bd0
Ragini Yadav <yadavragini446@gmail.com>
2026-08-25 17:01:21 +0530

    initial commit !

---
+++ react-demo-2/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(10);
+  //  let counter =10;
+
+  const addValue = () => {
+    console.log(`Value Added ! ${counter}`);
+    // counter=counter+1;
+    if(counter<20){
+      setCounter(counter + 1); 
+      
+    }
+   
+  };
+
+  const removeValue =()=>{
+    console.log(`Value Removed !${counter}`);
+     if(counter>0){
+      setCounter(counter - 1); 
+    }
+    // counter =counter-1;
+    // setCounter(counter);
+  };
+
+  return (
+    <>
+      <h1>Hello !</h1>
+      <h2>Counter value : {counter}</h2>
+      <p>
+        <button className="increase" onClick={addValue}>
+          Add Value :{counter}
+        </button>
+        <br />
+        <button className="decrease" onClick={removeValue}>
+          Remove Value :{counter}</button>
+      </p>
+    </>
+  );
+}
+
+export default App;
