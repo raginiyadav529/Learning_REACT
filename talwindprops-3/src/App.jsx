4b64d404480d8c3ced0556bd701b66f866de58c5
Ragini Yadav <yadavragini446@gmail.com>
2026-08-26 13:07:23 +0530

    initial commit !

---
+++ talwindprops-3/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HeroFeatures from "./components/Card";
+
+function App() {
+
+        let myObj =[1,2,3];
+
+  return (
+    <>
+      <HeroFeatures  username="Ragini Yadav"  btnText="Get Started"/>;
+    </>
+  );
+}
+
+export default App;
