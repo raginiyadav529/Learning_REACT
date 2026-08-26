3164977728496deebe8f7c839e87b585103c2af9
Ragini Yadav <yadavragini446@gmail.com>
2026-08-26 13:50:34 +0530

    heading added !

--- talwindprops-3/src/App.jsx
+++ talwindprops-3/src/App.jsx
@@ -2,12 +2,18 @@ import "./App.css";
 import HeroFeatures from "./components/Card";
 
 function App() {
-
-        let myObj =[1,2,3];
+  let myObj = [1, 2, 3];
 
   return (
     <>
-      <HeroFeatures  username="Ragini Yadav"  btnText="Get Started"/>;
+      <h1
+        className="bg-green-400 text-center p-3 hover:bg-pink-500 hover:text-white
+     text-3xl font-bold text-blue-900"
+      >
+        Tailwind Test
+      </h1>
+      <HeroFeatures username="Ragini Yadav" btnText="Get Started" />;
+      <HeroFeatures username="Modern Team" btnText="Learn more" />;
     </>
   );
 }
