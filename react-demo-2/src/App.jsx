78479566d471f2f6226b12ef35f6ad02bb4929af
Ragini Yadav <yadavragini446@gmail.com>
2026-08-26 20:09:59 +0530

    updated counter !

--- react-demo-2/src/App.jsx
+++ react-demo-2/src/App.jsx
@@ -3,23 +3,29 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(10);
+  let [counter, setCounter] = useState(5);
   //  let counter =10;
 
   const addValue = () => {
     console.log(`Value Added ! ${counter}`);
     // counter=counter+1;
-    if(counter<20){
-      setCounter(counter + 1); 
-      
+    if (counter < 20) {
+      setCounter(counter + 1);
+      setCounter(counter + 1); // single increment
+      setCounter(counter + 1);
+      setCounter(counter + 1);
+      /* setCounter((prevCounter) => prevCounter +1); 
+      setCounter((prevCounter) => prevCounter +1);  // multiple increment
+      setCounter((prevCounter) => prevCounter +1); 
+      setCounter((prevCounter) => prevCounter +1);
+      */
     }
-   
   };
 
-  const removeValue =()=>{
+  const removeValue = () => {
     console.log(`Value Removed !${counter}`);
-     if(counter>0){
-      setCounter(counter - 1); 
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
     // counter =counter-1;
     // setCounter(counter);
@@ -28,17 +34,18 @@ function App() {
   return (
     <>
       <h1>Hello ! ❤️</h1>
-      
+
       <h2>Keep Learning & Growing! ❤️</h2>
       <h2>Counter value : {counter}</h2>
       <p>
         <button className="increase" onClick={addValue}>
-          Add Value :{counter}
+          Add Value : {counter}
         </button>
         <br />
-        
+
         <button className="decrease" onClick={removeValue}>
-          Remove Value :{counter}</button>
+          Remove Value : {counter}
+        </button>
       </p>
     </>
   );
